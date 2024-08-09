ca824c684e1bbf1f0f8e815545d7e9a9d3bb03b9
Michael Gathwright <michael.gathwright@publix.com>
2024-08-09 13:01:04 -0400

    Configure request pipeline and IDP settings in Program.

--- ImageGallery.Client/Program.cs
+++ ImageGallery.Client/Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
-    .AddJsonOptions(configure => 
+    .AddJsonOptions(configure =>
         configure.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 // create an HttpClient used for accessing the API
@@ -13,6 +15,25 @@ builder.Services.AddHttpClient("APIClient", client =>
     client.BaseAddress = new Uri(builder.Configuration["ImageGalleryAPIRoot"]);
     client.DefaultRequestHeaders.Clear();
     client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+}).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme) // Congigures the cookie handler. Once an identity token is validate and transformed into a claims identity, it will be stored in a cookie.
+                                                                // The cookie is used on subsequent requests to the web app to check whether or not we are making an authenticated request. 
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme; // this registers and configures the OIDC handler. This handles create auth/token/idenity token validation requestions.
+    options.Authority = "https://localhost:5001"; // This is the address of our identity provider. The middleware uses this value to read the meta data on the discovery endpoint so it can find the different endpoints in the IDP.
+    options.ClientId = "imagegalleryclient"; // Will match the clientID on the IDP. Handled via back channel or server-to-server communication. 
+    options.ClientSecret = "secret"; // Also matches the secret in the IDP. The combo of secret and id ensures our client can do an authenticated call to the token endpoint.
+    options.ResponseType = "code"; // This corresponds to a grant time or code flow. Requires PKCE protection. Middleware automatically enables this when code is the response type.
+    //options.Scope.Add("openid"); // Already added by default.
+    //options.Scope.Add("profile"); // Already added by defuault. 
+    //options.CallbackPath = new PathString("signin-oidc"); // Use this to override or change the redirect URI.
+    options.SaveTokens = true; // Allows middleware to save tokens from the IDP so they can be used inside the client.
 });
 
 var app = builder.Build();
@@ -29,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Ensures all endpoints can be protected by authentcation. Goes after UseRouting (to allow calls to be routed to the endpoints), but will block any calls that are not Authenticated.
 app.UseAuthorization();
 
 app.MapControllerRoute(
